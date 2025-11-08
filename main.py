3256504be514c2b075c5c64a984d3af796e8cc75
Rohan <pudasainirohan0@gmail.com>
2025-11-08 22:19:23 +0545

    feat: refactor main.py to improve argument parsing and response handling and add simple message list for future

--- main.py
+++ main.py
@@ -1,20 +1,62 @@
+import argparse
 import os
 import sys
 
 from dotenv import load_dotenv
-from google import genai
 
-args = sys.argv[1:]
-if not args:
-    raise ValueError("Please provide at least one argument.")
-load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
-prompt = args[0]
+# from google import genai
+from google.genai import Client, types
 
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=prompt,
-)
-print("Response:", response.text)
-print("usages", response.usage_metadata)
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Simple Gemini AI Agent")
+    parser.add_argument(
+        "positional_message", nargs="?", help="Message to send to the AI"
+    )
+
+    parser.add_argument(
+        "--message", "-m", type=str, help="The message to send to the Gemini AI model"
+    )
+    parser.add_argument(
+        "--verbose", "-v", action="store_true", help="Enable verbose output"
+    )
+
+    args = parser.parse_args()
+    message = args.message or args.positional_message
+    return args, message
+
+
+def main():
+    args, message = parse_arguments()
+    is_verbose = args.verbose
+    prompt = message
+
+    if not prompt:
+        sys.exit(
+            "Please provide a message like `--message <your_message>` or `-m <your_message>` or <your_message>."
+        )
+    load_dotenv()
+
+    api_key = os.getenv("GEMINI_API_KEY")
+
+    client = Client(api_key=api_key)
+
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    if is_verbose:
+        print("User Prompt:", prompt)
+
+    print("Response:", response.text)
+    if not response.usage_metadata:
+        raise ValueError("Response is missing usage metadata.")
+    if is_verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
